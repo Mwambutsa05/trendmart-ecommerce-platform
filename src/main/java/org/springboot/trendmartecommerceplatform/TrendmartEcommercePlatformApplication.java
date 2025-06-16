7a9d86758e0efe37d547e02e5446344566b27253
mwambutsa05 <mwambutsadaryce@gmail.com>
2025-06-16 19:52:08 +0200

    first commit to check

---
+++ src/main/java/org/springboot/trendmartecommerceplatform/TrendmartEcommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.trendmartecommerceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendmartEcommercePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendmartEcommercePlatformApplication.class, args);
+    }
+
+}
