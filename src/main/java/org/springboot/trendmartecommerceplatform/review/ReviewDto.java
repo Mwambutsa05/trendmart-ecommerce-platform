af228ffec9b9cb1c2c147d22eaf3240dd1f2a1d1
sonia-trisher <soniatrisher86@gmail.com>
2025-06-24 11:16:36 +0200

    refactor: review less than or equal to 5 and Positive

--- src/main/java/org/springboot/trendmartecommerceplatform/review/ReviewDto.java
+++ src/main/java/org/springboot/trendmartecommerceplatform/review/ReviewDto.java
@@ -1,5 +1,7 @@
 package org.springboot.trendmartecommerceplatform.review;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -16,7 +18,8 @@ import lombok.ToString;
 public class ReviewDto  {
     private Long userId;
     private Long productId;
-    @Positive
+    @Positive(message = "Rating must be a positive")
+    @Max(value = 5, message = "Rating must be less that or equal to 5")
     private final int rating;
     @NotEmpty
     private final String comment;
