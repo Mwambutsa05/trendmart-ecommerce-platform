28efa7b9224674cf487c68b28825b212e025bf43
mwambutsa05 <mwambutsadaryce@gmail.com>
2025-06-25 12:30:11 +0200

    resolving conflicts

--- src/main/java/org/springboot/trendmartecommerceplatform/Product/Product.java
+++ src/main/java/org/springboot/trendmartecommerceplatform/Product/Product.java
@@ -7,11 +7,8 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springboot.trendmartecommerceplatform.category.Category;
 import org.springboot.trendmartecommerceplatform.stock.Stock;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.math.BigDecimal;
-import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -21,7 +18,7 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Setter
-public class Product implements UserDetails {
+public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -45,39 +42,5 @@ public class Product implements UserDetails {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "stock_id")
     private Stock stock;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
-
-    @Override
-    public String getPassword() {
-        return "";
-    }
-
-    @Override
-    public String getUsername() {
-        return "";
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
-    }
 }
+
