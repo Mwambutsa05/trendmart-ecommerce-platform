7b54b3937ac53eea37c3b77b52dbbab24f1b47e2
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-07-14 16:58:23 +0200

    resolve migration conficts

--- src/main/resources/db/migration/V8__create_wishlist_tables.sql
+++ src/main/resources/db/migration/V8__create_wishlist_tables.sql
@@ -215,3 +215,26 @@ ALTER TABLE wishlist_item
 
 ALTER TABLE product_image_urls
     ADD CONSTRAINT fk_product_imageurls_on_product FOREIGN KEY (product_id) REFERENCES product (id);
+
+
+CREATE SEQUENCE IF NOT EXISTS otp_verification_seq START WITH 1 INCREMENT BY 50;
+
+CREATE TABLE otp_verification
+(
+    id          BIGINT NOT NULL,
+    email       VARCHAR(255),
+    code        VARCHAR(255),
+    expiry_date TIMESTAMP WITHOUT TIME ZONE,
+    used        BOOLEAN,
+    CONSTRAINT pk_otp_verification PRIMARY KEY (id)
+);
+
+ALTER TABLE users
+    ADD verified BOOLEAN;
+
+ALTER TABLE users
+    ALTER COLUMN verified SET NOT NULL;
+
+
+DROP INDEX IF EXISTS idx_order_tracking_order_id;
+DROP INDEX IF EXISTS idx_order_tracking_status;
--- src/main/resources/db/migration/V9__add_payment_table.sql
+++ src/main/resources/db/migration/V9__add_payment_table.sql
@@ -12,3 +12,18 @@ CREATE TABLE payments
     payment_time   TIMESTAMP WITHOUT TIME ZONE,
     CONSTRAINT pk_payments PRIMARY KEY (id)
 );
+
+
+ALTER TABLE product_image_urls
+DROP
+CONSTRAINT fk_product_imageurls_on_product;
+
+ALTER TABLE users
+    ADD enabled BOOLEAN;
+
+ALTER TABLE users
+    ALTER COLUMN enabled SET NOT NULL;
+
+DROP TABLE product_image_urls CASCADE;
+
+DROP SEQUENCE reviews_seq CASCADE;
