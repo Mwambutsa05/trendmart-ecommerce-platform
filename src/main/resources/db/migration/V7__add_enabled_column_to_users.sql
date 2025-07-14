8b578bb365b11ff555f1ec76167d55abfc3b3599
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-07-14 17:08:44 +0200

    db: fix database migrations conflicts

--- src/main/resources/db/migration/V7__add_enabled_column_to_users.sql
+++
@@ -1,5 +0,0 @@
--- Add enabled column with default value and NOT NULL constraint in one step
-ALTER TABLE users ADD COLUMN enabled BOOLEAN NOT NULL DEFAULT TRUE;
-
--- Handle orders status column
-ALTER TABLE orders ALTER COLUMN status SET NOT NULL;
