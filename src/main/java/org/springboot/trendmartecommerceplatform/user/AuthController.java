a813473b0e1c82009e382b0316138c8833bf59eb
UAlice1 <umubyeyialice7@gmail.com>
2025-07-22 11:17:16 +0200

    pull

--- src/main/java/org/springboot/trendmartecommerceplatform/user/AuthController.java
+++ src/main/java/org/springboot/trendmartecommerceplatform/user/AuthController.java
@@ -52,6 +52,11 @@ public class AuthController {
         return ResponseEntity.ok(response);
     }
 
+    @PostMapping("/register/admin")
+    public ResponseEntity<String> registerAdmin(@Valid @RequestBody RegisterRequest request) {
+        userService.registerAdmin(request);
+        return ResponseEntity.ok("Admin registered. OTP sent to email.");
+    }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create-ADMIN")
@@ -66,18 +71,5 @@ public class AuthController {
         return ResponseEntity.ok("Deleted user with id " + id);
     }
 
-    // Optional: Uncomment if needed
-    /*
-    @PostMapping("/verify/send")
-    public ResponseEntity<String> sendCode(@RequestParam String email) {
-        emailService.sendVerificationCode(email);
-        return ResponseEntity.ok("Verification code sent.");
-    }
 
-    @PostMapping("/verify/check")
-    public ResponseEntity<String> checkCode(@RequestParam String email, @RequestParam String code) {
-        boolean isValid = emailService.verifyOtp(email, code);
-        return isValid ? ResponseEntity.ok("Verified!") : ResponseEntity.badRequest().body("Invalid or expired code.");
-    }
-    */
 }
